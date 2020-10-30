862e7f02bcb2861578092391449395176ce3b9fd
GK <gkamperis@msn.com>
2020-10-30 12:37:10 +0000

    added repro

---
+++ libs/test-lib/src/lib/test-component/test-component.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'test-ws-test-component',
+  templateUrl: './test-component.component.html',
+  styleUrls: ['./scss/test-component.component.scss'],
+})
+export class TestComponentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ libs/test-lib/src/lib/test-lib.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestComponentComponent } from './test-component/test-component.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [TestComponentComponent],
+  exports: [TestComponentComponent],
+})
+export class TestLibModule {}
